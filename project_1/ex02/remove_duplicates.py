46eb1080b67d97af0e0afb2c3e33bd83de0c096d
Rodrigo Girondo <rgirondo@c3r13s4.42madrid.com>
2025-04-17 16:53:45 +0200

    customer_table y remove_duplicates hechos

--- project_1/ex02/remove_duplicates.py
+++ project_1/ex02/remove_duplicates.py
@@ -8,17 +8,46 @@ cursor = connect.cursor()
 
 
 cursor.execute("""
-                DELETE FROM customers a
-                USING customers b
-                WHERE a.ctid < b.ctid
-                AND a.event_type = b.event_type
-                AND a.product_id = b.product_id
-                AND a.price = b.price
-                AND a.user_id = b.user_id
-                AND a.user_session = b.user_session
-                ;
+                DROP TABLE IF EXISTS Customers_Temp;
+                CREATE TABLE Customers_Temp AS
+                WITH Temp AS (
+                    SELECT *,
+                        LAG(event_time) OVER (
+                            PARTITION BY event_type, product_id, price, user_id, user_session
+                            ORDER BY event_time
+                        ) AS previous_time
+                    FROM customers
+                ),
+                No_Lag AS (
+                    SELECT *
+                    FROM Temp
+                    WHERE previous_time IS NULL
+                    OR event_time - previous_time > interval '1 seconds'
+                    ORDER BY event_time
+                )
+                SELECT * FROM No_Lag;
                 """)
 
 connect.commit()
+
+cursor.execute("""
+                DROP TABLE customers;
+                ALTER TABLE Customers_Temp RENAME TO Customers;
+                """)
+
+connect.commit()
+
+cursor.execute("""
+                ALTER TABLE Customers;
+                DROP COLUMN previous_time
+                """)
+
+connect.commit()
+
 cursor.close()
 connect.close()
+
+#For Evaluation
+#SELECT * FROM public.customers WHERE product_id = 5809103 AND event_type = 'remove_from_cart' AND event_time = '2022-10-01 00:00:30'
+
+#Rows 19175899
