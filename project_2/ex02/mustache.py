a1c912920c25d6b476b7ad202c6df83b5dbd3309
Rodrigo Girondo <rgirondo@c2r15s1.42madrid.com>
2025-04-21 14:08:45 +0200

    box charts terminadas a lo guarro

--- project_2/ex02/mustache.py
+++ project_2/ex02/mustache.py
@@ -3,33 +3,54 @@ import psycopg2 as psy
 import matplotlib.dates as mdates
 import numpy as np
 
-def make_box_plot(data : list) :
+def make_box_plot1(data : list) :
+
+    axisX : list = []
+
+    for i in data:
+        axisX.append(i[0])
+    
+    fig, box_plot = plt.subplots(figsize=(5, 5))
+    box_plot.boxplot(axisX, vert=False, boxprops=dict(facecolor="lightgreen"), patch_artist=True)
+    box_plot.grid(True, axis='x')
+
+    plt.tight_layout()
+
+def make_box_plot2(data : list) :
 
     axisX : list = []
 
     for i in data:
         axisX.append(i[1])
     
-    plt.figure(figsize=(8, 5))
-    plt.boxplot(axisX, vert=False, boxprops=dict(facecolor="lightblue"), patch_artist=True, whis=0.20)
+    fig, box_plot = plt.subplots(figsize=(5, 5))
+    box_plot.boxplot(axisX, vert=False, boxprops=dict(facecolor="lightblue"), patch_artist=True)
+    box_plot.grid(True, axis='x')
 
-    plt.xticks(np.arange(int(min(axisX)), int(max(axisX)) + 1, step=2)) 
-    plt.grid(True, axis='x')
-    plt.xlim(min(axisX) - 1, max(axisX) + 1)
     plt.tight_layout()
-    plt.yticks([])
-    plt.show()
+
+def get_box1_data() -> list :
+
+    data : list = []
+
+    cursor.execute(f"""
+                    SELECT price
+                    FROM customers
+                    WHERE event_type = 'purchase'
+                    """)
+    data = cursor.fetchall()
+    return data
+
 
 def get_box2_data() -> list :
 
     data : list = []
 
     cursor.execute(f"""
-                    SELECT user_id, AVG(price) AS avg_cart_price
+                    SELECT user_id, SUM(price) AS avg_cart_price
                     FROM customers
                     WHERE event_type = 'purchase'
-                    GROUP BY user_id
-                    HAVING AVG(price) BETWEEN 26 AND 43;
+                    GROUP BY user_id, user_session
                     """)
     data = cursor.fetchall()
     return data
@@ -37,9 +58,29 @@ def get_box2_data() -> list :
 connect = psy.connect(host="localhost", dbname="piscineds", user="rgirondo", password="mysecretpassword")
 cursor = connect.cursor()
 
-#Grafica de lineas Clientes / Dia
-box_data = get_box2_data()
-make_box_plot(box_data)
+box_data = get_box1_data()
+print_data = []
 
+for i in box_data:
+    print_data.append(i[0])
+
+#Imprimir datos derivados del precio de los objetos
+print(f"{'count':<8} {len(print_data):>15.6f}")
+print(f"{'mean':<8} {np.mean(print_data):>15.6f}")
+print(f"{'std':<8} {np.std(print_data):>15.6f}")
+print(f"{'min':<8} {np.min(print_data):>15.6f}")
+print(f"{'25%':<8} {np.percentile(print_data, 25):>15.6f}")
+print(f"{'50%':<8} {np.percentile(print_data, 50):>15.6f}")
+print(f"{'75%':<8} {np.percentile(print_data, 75):>15.6f}")
+print(f"{'max':<8} {np.min(print_data):>15.6f}")
+
+#Box plot del precio de los objetos comprados
+make_box_plot1(box_data)
+
+#Box plot cesta de la compra media por usuario
+box_data = get_box2_data()
+make_box_plot2(box_data)
+
+plt.show()
 cursor.close()
 connect.close()
